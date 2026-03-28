51d77e6528bf0bb4a79db06e0c60815d8ce2acce
Philani Jack Ram <philanijackr@gmail.com>
2026-03-28 23:11:22 +0200

    First Commit

---
+++ app/src/main/java/com/jacks_lan/learningappjetpackcompose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.jacks_lan.learningappjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jacks_lan.learningappjetpackcompose.ui.theme.LearningAppJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearningAppJetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "SOFTWARE ENGINEERING",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LearningAppJetpackComposeTheme {
+        Greeting()
+    }
+}
